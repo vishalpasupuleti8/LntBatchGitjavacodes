0bdbf72fa6c1385c567587e7ce4c2f994289b3a3
vishalpasupuleti8 <vishalpasupuleti8@gmail.com>
2022-03-02 00:03:13 +0530

    My First Commit

---
+++ GitTest_Project/src/com/lnt/GitCodes/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.lnt.GitCodes;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi......");
+
+	}
+
+}
